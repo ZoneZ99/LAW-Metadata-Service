103c7654f87625f144dcaec9f9d86ca6d1de053f
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-03-04 01:20:36 +0700

    Add description field

--- service/models.py
+++ service/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class FileMetadata(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, null=True)
     type = models.CharField(max_length=10)
     size = models.PositiveIntegerField()
     location = models.URLField()
--- service/serializers.py
+++ service/serializers.py
@@ -6,5 +6,15 @@ from service.models import FileMetadata
 class FileMetadataSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FileMetadata
-        fields = ["url", "id", "name", "type", "size", "location", "created", "updated"]
+        fields = [
+            "url",
+            "id",
+            "name",
+            "description",
+            "type",
+            "size",
+            "location",
+            "created",
+            "updated",
+        ]
         read_only_fields = ["url", "id", "created"]
